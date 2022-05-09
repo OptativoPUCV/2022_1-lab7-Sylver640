b083d226a1081bb6034dc7bc9b3d953df3351a9d
Your Name <you@example.com>
2022-05-08 22:36:06 -0400

    new try

--- heap.c
+++ heap.c
@@ -53,9 +53,9 @@ void heap_push(Heap* pq, void* data, int priority){
 void heap_pop(Heap* pq){
    heapElem* aux = (heapElem*) malloc (sizeof(heapElem));
 
-   *aux = pq->heapArray[0];
+   //*aux = pq->heapArray[0];
    pq->heapArray[0] = pq->heapArray[pq->size];
-   pq->heapArray[pq->size] = *aux;
+   //pq->heapArray[pq->size] = *aux;
    pq->size--;
 
    int auxSize = 0;
